fe0949e521d3cdc8b8bb624ef3fee2d3fede7920
Samir Ezequiel <samirgoede35@gmail.com>
2025-08-22 22:15:24 -0400

    Armando estructura del proyecto

---
+++ gestion_de_tareas/src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <h2 className="text-xl">Tu tablero de proyectos</h2>
+      </div>
+    );
+  }
+  
